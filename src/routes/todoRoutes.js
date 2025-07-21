a25ce237e28f3d3e49c961d131558e4592f70f87
IpshitaArora <arora.ipshita43@gmail.com>
2025-07-21 15:24:48 +0530

    add new routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -44,3 +44,4 @@ router.delete("/:id", auth, roleCheck(["admin"]), async (req, res) => {
 });
 
 module.exports = router;
+``
--- src/server.js
+++ src/server.js
@@ -4,13 +4,30 @@ const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
+// const userRoutes = require("./routes/userRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// console.log("authRoutes:", authRoutes);
+// console.log("todoRoutes:", todoRoutes);
+// console.log("userRoutes:", userRoutes);
+// console.log("errorHandler:", errorHandler);
+
+
+// DB Connection
 connectDB();
+
+// Middleware
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
+// app.use("/api/users", userRoutes);
+
+// Error Handler
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
